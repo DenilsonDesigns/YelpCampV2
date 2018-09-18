6ced14cdfeaadbe65f3c63d06e043c557f1b401a
DenilsonDesigns <daniellanguilller@gmail.com>
2018-09-18 23:15:31 +1000

    setup databaseurl.env

--- app.js
+++ app.js
@@ -17,9 +17,7 @@ const campgroundRoutes = require("./routes/campgrounds");
 const indexRoutes = require("./routes/index");
 
 // mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect(
-  "mongodb://DenilsonReacts:Coffee12@ds161062.mlab.com:61062/yelpcamp"
-);
+mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
